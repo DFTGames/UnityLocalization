1959a1d3b04956e645f901699e9cb5a2665119d1
Pino De Francesco <PinoEire@users.noreply.github.com>
2026-02-22 10:11:18 +0000

    Improved PowerShell script for generating the UTF8 file for TextMeshPro

--- Assets/DFTGames/Localization/Demo/Resources/localization/mergeAllText.ps1
+++ Assets/DFTGames/Localization/Demo/Resources/localization/mergeAllText.ps1
@@ -1,1 +1,14 @@
-(gc -Encoding Utf8 *.txt) | out-file AllText.txt -encoding utf8
+$utf8NoBom = New-Object System.Text.UTF8Encoding($false)  # $false => no BOM
+$outPath   = "AllText.txt"
+
+$sw = New-Object System.IO.StreamWriter($outPath, $false, $utf8NoBom)
+try {
+    Get-ChildItem -Filter *.txt -File | ForEach-Object {
+        Get-Content -LiteralPath $_.FullName -Encoding UTF8 | ForEach-Object {
+            $sw.WriteLine($_)
+        }
+    }
+}
+finally {
+    $sw.Dispose()
+}
